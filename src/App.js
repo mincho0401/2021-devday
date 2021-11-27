e75224321db1569147ac7771dc4b0af3efb41196
mincho0401 <mincho@kmong.com>
2021-11-27 20:06:48 +0900

    소감추가 및 처음 화면 돌아가는 듯하게 보이도록 메뉴 하나 추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function DevDayPage() {
   const [input, setInput] = useState('');
   const xtermRef = useRef(null);
   const writeTerminal = useCallback((text) => xtermRef.current?.terminal.write(text), []);
+  const clearTerminal = useCallback(() => xtermRef.current?.terminal.clear());
 
   const asyncTyped = useCallback(async (text, time = 200) => {
     return new Promise((resolve) => {
@@ -71,41 +72,8 @@ function DevDayPage() {
         }
 
         clearTerminal();
-        await asyncTyped(' ', 500);
-        writeTerminal('\r\n');
-        writeTerminal('\r\n');
-
-        writeTerminal('\r\t*************	 Kmong DevDay	 *************\n');
-        await asyncTyped(' ', 750);
-        writeTerminal(`\r\t\t      일정표 D-${day} (12/16)\n`);
-        await asyncTyped(' ', 250);
-
-        writeTerminal('\r\n');
-        writeTerminal('\r\n');
-
-        await asyncTyped(' ', 550);
-        writeTerminal('\r\t1부\n');
-        writeTerminal('\r\t\t14:00 - 14:30   ooo  [주제 주제 주제]\n');
-        writeTerminal('\r\t\t14:40 - 15:10   ooo  [주제 주제 주제]\n');
-        writeTerminal('\r\t\t15:20 - 15:50   ooo  [주제 주제 주제]\n');
-        
-
-        writeTerminal('\r\n');
-        writeTerminal('\r\n');
-
-        await asyncTyped(' ', 550);
-        writeTerminal('\r\t쉬는 시간\n');
-        writeTerminal('\r\t\t15:50 - 16:10    쉬고 오셔용 (｡•̀ᴗ-)✧ (찡긋-☆)\n');
-
-        writeTerminal('\r\n');
-        writeTerminal('\r\n');
-
-        await asyncTyped(' ', 550);
-        writeTerminal('\r\t2부\n');
-        writeTerminal('\r\t\t16:10 - 16:40    ooo [주제 주제 주제]\n');
-        writeTerminal('\r\t\t16:50 - 17:20    ooo [주제 주제 주제]\n');
-        writeTerminal('\r\t\t17:30 - 18:00    ooo [주제 주제 주제]\n');
-
+       
+        writeSchedule(day);
 
         break;
       }
@@ -116,22 +84,17 @@ function DevDayPage() {
       case 'cd':
       case 'cd ':
       case 'cd ..':
+      case 'cd ~' : {
+        window.location.replace("/")
+        break;
+      }
       case 'cd bk':
       case 'cd edgar':
       case 'cd daniel':
       case 'cd mincho': {
 
-      await asyncTyped('', 500);
-      await asyncTyped('안');
-      await asyncTyped('녕');
-      await asyncTyped('하');
-      await asyncTyped('세');
-      await asyncTyped('요', 150);
-      await asyncTyped('.', 150);
-      await asyncTyped('.', 150);
-      await asyncTyped('\r\n', 150);
-      await asyncTyped('이 글을 보고 있다면 ... ', 150);
-
+        clearTerminal();
+        writeMincho();
 
         break;
       }
@@ -217,17 +180,25 @@ function DevDayPage() {
       writeTerminal('\r\n');
     }
 
-    function writeReadme() {
+    async function writeReadme() {
       writeTerminal(WITH_PROMPT);
       const url = HELP_LIST.find((help) => help.command === 'devday readme').url;
       writeTerminal(`visit to ${url}`);
+
+      await asyncTyped('\r\n', 150);
+      writeTerminal('\r\n');
+      asyncTyped('press devday --help to show menu\n\r');
     }
 
     async function writeStart() {
       writeTerminal(WITH_PROMPT);
       const url = HELP_LIST.find((help) => help.command === 'devday start').url;
       writeTerminal(`visit to ${url}`);
-      setEnabled(false);
+      writeTerminal('\r\n');
+      await asyncTyped('password : kmong', 300);
+
+      // 움직일 수가 없다 ... 
+      // setEnabled(false);
       writeTerminal('\r\n');
       await asyncTyped('.', 300);
       writeTerminal('\r\n');
@@ -240,8 +211,82 @@ function DevDayPage() {
       await asyncTyped('.....', 900);
       writeTerminal('\r\n');
       writeTerminal("[프로세스 완료됨]");
+
+      await asyncTyped('\r\n', 150);
+      writeTerminal('\r\n');
+      asyncTyped('press devday --help to show menu\n\r');
+
     }
   }, [asyncTyped, input, writeTerminal]);
+
+  async function writeSchedule(day) {
+    await asyncTyped('', 500);
+    writeTerminal('\r\n');
+    writeTerminal('\r\n');
+
+    writeTerminal('\r\t*************	 Kmong DevDay	 *************\n');
+    await asyncTyped('', 750);
+    writeTerminal(`\r\t\t      일정표 D-${day} (12/16)\n`);
+    await asyncTyped('', 250);
+
+    writeTerminal('\r\n');
+    writeTerminal('\r\n');
+
+    await asyncTyped('', 550);
+    writeTerminal('\r\t1부\n');
+    writeTerminal('\r\t\t14:00 - 14:30   ooo  [주제 주제 주제]\n');
+    writeTerminal('\r\t\t14:40 - 15:10   ooo  [주제 주제 주제]\n');
+    writeTerminal('\r\t\t15:20 - 15:50   ooo  [주제 주제 주제]\n');
+    
+
+    writeTerminal('\r\n');
+    writeTerminal('\r\n');
+
+    await asyncTyped('', 550);
+    writeTerminal('\r\t쉬는 시간\n');
+    writeTerminal('\r\t\t15:50 - 16:10    쉬고 오셔용 (｡•̀ᴗ-)✧ (찡긋-☆)\n');
+
+    writeTerminal('\r\n');
+    writeTerminal('\r\n');
+
+    await asyncTyped('', 550);
+    writeTerminal('\r\t2부\n');
+    writeTerminal('\r\t\t16:10 - 16:40    ooo [주제 주제 주제]\n');
+    writeTerminal('\r\t\t16:50 - 17:20    ooo [주제 주제 주제]\n');
+    writeTerminal('\r\t\t17:30 - 18:00    ooo [주제 주제 주제]\n');
+
+    writeTerminal('\r\n');
+    asyncTyped('press devday --help to show menu\n\r');
+    writeTerminal('\r\n');
+  }
+
+  // 이런식으로 소감 넣어도 좋을것 같아용
+  async function writeMincho(){
+
+      await asyncTyped('', 500);
+      await asyncTyped('안');
+      await asyncTyped('녕');
+      await asyncTyped('하');
+      await asyncTyped('세');
+      await asyncTyped('요', 150);
+      await asyncTyped('.', 150);
+      await asyncTyped('.', 150);
+      await asyncTyped('\r\n', 150);
+      await asyncTyped('이 글을 보고 있다면 ... ', 150);
+
+      await asyncTyped('\r\n', 150);
+      await asyncTyped('우린 회의를 ... ', 550);
+      await asyncTyped('\r\n', 150);
+      await asyncTyped('하고 있겠죠.. ??', 550);
+      await asyncTyped('\r\n', 150);
+      await asyncTyped('그럼 전 이만', 550);
+      await asyncTyped('\r\n', 150);
+      await asyncTyped('뿅 !! (〜￣▽￣)〜', 550);
+      await asyncTyped('\r\n', 150);
+      writeTerminal('\r\n');
+      asyncTyped('press devday --help to show menu\n\r');
+
+  }
 
   const handleChangeCLI = useCallback((data) => {
     if (!enabled) {
